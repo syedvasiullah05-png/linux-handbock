9c3b7794b0627908aedfc6eb6d477a714e66fd98
syedvasiullah05-png <syedvasiullah05@gmail.com>
2025-12-02 12:52:57 +0530

    Start and enable Nginx after installation
    Added commands to start and enable Nginx after installation.

--- linux-server-setup/level-1-basic/packages/install_packages.sh
+++ linux-server-setup/level-1-basic/packages/install_packages.sh
@@ -3,13 +3,18 @@
 echo "Installing Nginx..."
 
 if command -v apt >/dev/null; then
-
+    sudo apt update
+    sudo apt install nginx -y
 elif command -v yum >/dev/null; then
     sudo yum install nginx -y
-
 else
     echo "OS not supported"
     exit 1
 fi
+
+# Start and enable Nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
+
+echo "Nginx installation completed."
+
