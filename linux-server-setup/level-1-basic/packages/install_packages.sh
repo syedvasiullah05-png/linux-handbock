c97dd70ee19340d5708550d7a67899c7c7cca9f2
syedvasiullah05-png <syedvasiullah05@gmail.com>
2025-12-02 19:21:33 +0530

    Update install_packages.sh

--- linux-server-setup/level-1-basic/packages/install_packages.sh
+++ linux-server-setup/level-1-basic/packages/install_packages.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-
-echo "Installing Nginx..."
-
 if command -v apt >/dev/null; then
-    sudo apt update
-    sudo apt install nginx -y
-elif command -v yum >/dev/null; then
-    sudo yum install nginx -y
-else
-    echo "OS not supported"
-    exit 1
+  sudo apt update
+ sudo yum install -y git nginx
+ else
+ udo dnf install -y git nginx
 fi
 
-# Start and enable Nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx
-
-echo "Nginx installation completed."
-
+sudo systemctl enable --now nginx
